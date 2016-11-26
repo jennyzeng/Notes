0661ebfe459738dcffc3d20e646064e951255c8c
jennyzeng <jennyzengzzh@gmail.com>
2016-11-26 18:14:46 -0500

    better inplace quicksort

--- algorithmCodes/inPlaceQuickSort.py
+++ algorithmCodes/inPlaceQuickSort.py
@@ -6,36 +6,39 @@
 import random
 
 
+# i is start and j is stop index
 def quickSort(i, j):
 	if j - i + 1 <= 1:
 		return
 	k = random.randint(i, j)
-	L[k], L[i] = L[i], L[k]  # swap, L[i] is pivot
+	swap(L, k, i)# swap, L[i] is pivot
 	k = partition(i, j)
-	if k - 1 > i:
-		quickSort(i, k - 1)
-	if k + 1 < j:
-		quickSort(k + 1, j)
+	quickSort(i, k - 1)
+	quickSort(k + 1, j)
 
 
 def partition(i, j):
 	s = i + 1
 	e = j
 	while s <= e:
-		while s <= e and L[s] < L[i]:
+		if L[s] <= L[i]:
 			s += 1
-		while s <= e and L[e] > L[i]:
+		elif L[e] >= L[i]:
 			e -= 1
-		if s > e:
-			break
-		L[s], L[e] = L[e], L[s]
-		s += 1
-		e -= 1
-	L[i], L[s - 1] = L[s - 1], L[i]
-	return s-1
+		else:
+			swap(L, s, e)
+			s += 1
+			e -= 1
+	swap(L, i, s-1)
+	return s - 1
+
+
+def swap(L, i, j):
+	L[i], L[j] = L[j], L[i]
+
 
 L = [0, 9, 4, 1, 5, 2, 7]
-quickSort(0, len(L)-1)
+quickSort(0, len(L) - 1)
 print(L)
 L = [1, 1, -2, 2, 3, 4, 1, 2, 5, 0, -1]
 quickSort(0, len(L) - 1)
